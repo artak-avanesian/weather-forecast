1701e7a04fb53475194a40f6cfa897c3aa27a633
Artak <artak.avanesyan93@gmail.com>
2019-03-29 00:52:29 +0200

    Updates

---
+++ precache-manifest.579a6a11c981a39e821b815375c27b39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e988ba8af13e088622f38102e9e83a72",
+    "url": "/weather-forecast/static/media/Glyphter3.e988ba8a.ttf"
+  },
+  {
+    "revision": "11757b7861133ea9494c323e8de3ae27",
+    "url": "/weather-forecast/static/media/Glyphter2.11757b78.ttf"
+  },
+  {
+    "revision": "a372b5f234d79bb18b808f889026f4e5",
+    "url": "/weather-forecast/static/media/Glyphter.a372b5f2.ttf"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "71cfa5e44fa721ad4097",
+    "url": "/weather-forecast/static/js/main.f90d3df8.chunk.js"
+  },
+  {
+    "revision": "aee7c5d8d3ad6db5c482",
+    "url": "/weather-forecast/static/js/2.aaf390f1.chunk.js"
+  },
+  {
+    "revision": "71cfa5e44fa721ad4097",
+    "url": "/weather-forecast/static/css/main.ed04a307.chunk.css"
+  },
+  {
+    "revision": "aee7c5d8d3ad6db5c482",
+    "url": "/weather-forecast/static/css/2.1a67b926.chunk.css"
+  },
+  {
+    "revision": "227fcb66053ac912dd031b3236ab853c",
+    "url": "/weather-forecast/index.html"
+  }
+];
